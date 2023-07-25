38ac29be250a83f329c0f41c376bf0657e52d842
Yara <kavaarinka@gmail.com>
2023-07-25 19:28:56 +0300

    Initial Commit

---
+++ DocsExample/DocsExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocsExampleApp.swift
+//  DocsExample
+//
+//  Created by Yaro4ka on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct DocsExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
