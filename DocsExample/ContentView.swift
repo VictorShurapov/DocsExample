0b0a5363a17ff4d5e1f971834a331b601be3cb2c
Viktor <vshurapov@gmail.com>
2023-08-21 17:05:59 +0300

    Presentation background - with ShapeStyle and custom View

--- DocsExample/ContentView.swift
+++ DocsExample/ContentView.swift
@@ -10,35 +10,38 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            Section("Alerts") {
-                NavigationLink("Alert with Error", destination: AlertWithErrorView())
-                NavigationLink("Alert with Error presenting data", destination: TicketPurchase())
-                    .navigationTitle("Docs Examples")
-                NavigationLink("Alert with message", destination: AlertMessageView())
-                NavigationLink("Alert with data", destination: SaveButton())
-                    .navigationTitle("Docs Examples")
-            }
-            Section("Confirmations") {
-                NavigationLink("Confirmation", destination: ConfirmEraseItems())
-                NavigationLink("Confirmation with message", destination: ConfirmEraseItemsMessage())
-                NavigationLink("Confirmation with data", destination: ConfirmFileImport())
-                NavigationLink("Confirmation with data and message", destination: ConfirmFileImportMessage())
-            }
-            Section("Sheets") {
-                NavigationLink("Sheet Binding to Bool value", destination: ShowLicenseAgreement())
-                NavigationLink("Sheet Binding to Data Source", destination: ShowPartDetail())
-            }
-            Section("Full-Screen Cover") {
-                NavigationLink("Full-Screen Cover Binding to Bool value", destination: FullScreenCoverPresentedOnDismiss())
-                NavigationLink("Full-Screen Cover Binding to Data Source", destination: FullScreenCoverItemOnDismissContent())
-            }
-            Section("Popovers") {
-                NavigationLink("Popover with Binding to Bool value", destination: PopoverExample())
-                NavigationLink("Popover with Binding to Data Source", destination: PopoverExampleItem())
-            }
-            Section("Sheet and popover configuration") {
-                NavigationLink("Interactive Dismiss Disabled", destination: PresentationInteractiveDismiss())
-                NavigationLink("Presentation Detents", destination: PresentationDetents())
+            List {
+                Section("Alerts") {
+                    NavigationLink("Alert with Error", destination: AlertWithErrorView())
+                    NavigationLink("Alert with Error presenting data", destination: TicketPurchase())
+                        .navigationTitle("Docs Examples")
+                    NavigationLink("Alert with message", destination: AlertMessageView())
+                    NavigationLink("Alert with data", destination: SaveButton())
+                        .navigationTitle("Docs Examples")
+                }
+                Section("Confirmations") {
+                    NavigationLink("Confirmation", destination: ConfirmEraseItems())
+                    NavigationLink("Confirmation with message", destination: ConfirmEraseItemsMessage())
+                    NavigationLink("Confirmation with data", destination: ConfirmFileImport())
+                    NavigationLink("Confirmation with data and message", destination: ConfirmFileImportMessage())
+                }
+                Section("Sheets") {
+                    NavigationLink("Sheet Binding to Bool value", destination: ShowLicenseAgreement())
+                    NavigationLink("Sheet Binding to Data Source", destination: ShowPartDetail())
+                }
+                Section("Full-Screen Cover") {
+                    NavigationLink("Full-Screen Cover Binding to Bool value", destination: FullScreenCoverPresentedOnDismiss())
+                    NavigationLink("Full-Screen Cover Binding to Data Source", destination: FullScreenCoverItemOnDismissContent())
+                }
+                Section("Popovers") {
+                    NavigationLink("Popover with Binding to Bool value", destination: PopoverExample())
+                    NavigationLink("Popover with Binding to Data Source", destination: PopoverExampleItem())
+                }
+                Section("Sheet and popover configuration") {
+                    NavigationLink("Interactive Dismiss Disabled", destination: PresentationInteractiveDismiss())
+                    NavigationLink("Presentation Detents", destination: PresentationDetents())
+                    NavigationLink("Presentation Background", destination: PresentationBackground())
+                }
             }
         }
     }
---
+++ DocsExample/PresentationBackground.swift
@@ -0,0 +1,57 @@
+//
+//  PresentationBackground.swift
+//  DocsExample
+//
+//  Created by Yaro4ka on 21.08.2023.
+//
+
+import SwiftUI
+
+struct PresentationBackground: View {
+    // present sheet with shape style or custom view as background
+    @State private var showSheetShapeStyle = false
+    @State private var showSheetCustomView = false
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(.green)
+                .frame(width: 300, height: 300)
+            VStack(spacing: 20) {
+                Button("Sheet with shape style background") {
+                    showSheetShapeStyle = true
+                }
+                .sheet(isPresented: $showSheetShapeStyle) {
+                    Text("Shape style background")
+                        .presentationBackground(.ultraThinMaterial)
+                }
+                
+                Button("Sheet with custom view background") {
+                    showSheetCustomView = true
+                }
+                .sheet(isPresented: $showSheetCustomView) {
+                    Text("Custom view background")
+                        .presentationBackground {
+                            TestView()
+                        }
+                }
+            }
+        }
+    }
+}
+
+struct TestView: View {
+    
+    var body: some View {
+        ZStack {
+            Rectangle().fill(.red)
+            Rectangle().fill(.ultraThinMaterial)
+        }
+    }
+}
+
+struct PresentationBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        PresentationBackground()
+    }
+}
